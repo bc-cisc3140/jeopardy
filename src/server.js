0580b8b51e3297656e3427df4a711849912d44ab
Nick <35080025+iEthix@users.noreply.github.com>
2023-12-09 17:32:02 -0500

    Update server.js
    -Changed port
    -Added a route to fetch the gameboard page

--- src/server.js
+++ src/server.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 const cors = require('cors');
+const path = require('path'); // Make sure to require the path module
+
 const app = express();
 app.use(cors());
-app.use(express.static('public'));
+app.use(express.static('public')); // Serves files from 'public' as static files
 
-const db = new sqlite3.Database('./database.db', sqlite3.OPEN_READWRITE, (err) => {
+const db = new sqlite3.Database('C:\\Users\\nicho\\OneDrive\\Desktop\\Projects\\Jeopardy\\jeopardy.db', sqlite3.OPEN_READWRITE, (err) => {
     if (err) {
         console.error(err.message);
     }
     console.log('Connected to the jeopardy database.');
 });
 
+// Route for fetching a specific question
 app.get('/get-question', (req, res) => {
   const qid = req.query.id;
   db.get('SELECT prompt FROM jeopardy WHERE qid = ?', [qid], (err, row) => {
@@ -22,7 +25,12 @@ app.get('/get-question', (req, res) => {
   });
 });
 
-const PORT = 3000;
+// Route for serving the homepage
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'gameboard.html'));
+});
+
+const PORT = 8000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
