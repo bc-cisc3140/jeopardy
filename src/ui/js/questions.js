f6a56bf933a38b274b4466e26a8aece12a732637
Nick <35080025+iEthix@users.noreply.github.com>
2023-12-09 16:45:40 -0500

    Update questions.js
    -Retrieves the question id from the selected jeopardy square from the gameboard.
    -When retrieving the question id from the gameboard square, we also retrieve the respected question stored within the database that contains the same id.
    -showQuestionModal() function to display the modal once a jeopardy square is selected.

--- src/ui/js/questions.js
+++ src/ui/js/questions.js
@@ -1,8 +1,28 @@
-async function showQuestions() {
-  let response = await fetch("/api/fruits");
-  let fruits = await response.json();
-  //console.log(fruits.data);
+document.querySelectorAll('.jeopardy-square').forEach(square => {
+  square.addEventListener('click', function() {
+      const questionId = this.dataset.questionId;
+      fetchQuestion(questionId); //Fetch question ID
+  });
+});
 
-  //load into webpage
-  document.getElementById("fruits").innerText = fruits.data.map(a => a.name).join(", ");
+//Retrieve the question to input into the modal
+function fetchQuestion(questionId) {
+  fetch(`http://localhost:3000/get-question?id=${questionId}`)
+      .then(response => response.json())
+      .then(questionData => {
+          showQuestionModal(questionData.prompt);
+      })
+      .catch(error => {
+          console.error('Error:', error);
+      });
+}
+
+//Function to display the modal
+function showQuestionModal(questionText) {
+  const questionModalElement = document.getElementById('questionModal');
+  const modalBody = questionModalElement.querySelector('.modal-body');
+  modalBody.textContent = questionText;
+
+  var questionModal = new bootstrap.Modal(questionModalElement);
+  questionModal.show();
 }
