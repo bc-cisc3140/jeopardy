ef3c449df1ad8f732ed378882d5387cf3a8ff186
Nick <35080025+iEthix@users.noreply.github.com>
2023-12-10 17:27:16 -0500

    Update sample.sql
    Added some questions and categories to the gameboard

--- src/sql/sample.sql
+++ src/sql/sample.sql
@@ -9,8 +9,12 @@ CREATE TABLE jeopardy (
 );
 /*Sample questions for testing, will add more overtime*/
 INSERT INTO jeopardy (topic, level, prompt, answer) VALUES
-('Software Types', 100, 'This kind of software is made for end users, typically for business applications.', 'What are software applications?'),
-('Historical Events', 200, 'This event occurred on July 20, 1969.', 'What is the moon landing?'),
-('Historical Events', 300, 'This event occurred on July 20, 1969.', 'What is the moon landing?');
+('History of Computing', 100, 'This ancient computing device, consisting of beads on rods, was used for arithmetic calculations and is still in use in some parts of the world today.', 'What is an abacus?'),
+('History of Computing', 200, 'Method was used to feed instructions into early computing machines', 'What are punch cards?'),
+('History of Computing', 300, 'Who is considered to be the father of computers?', 'Who is Charles Babbage?'),
+('History of Computing', 400, 'What was the name of the German cypher machine used in World War II?', 'What is the Engima?'),
+('History of Computing', 500, 'Considered the first woman programmer?', 'Who is Ada Lovelace?'),
+('Terminal Environment', 100, 'What year was UNIX developed?', 'What is 1969?'),
+('Terminal Environment', 200, 'What structure is your file directory?', 'What is a tree?');
 
 SELECT * FROM jeopardy;
