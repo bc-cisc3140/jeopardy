ca8a0af1ca53df7da84d968c1ee0379e2be43cf8
yisroel <yisroelshulman16@gmail.com>
2023-12-10 19:36:53 -0500

    added the .sql file to create the table and import from the csv file

---
+++ src/sql/jeopardy.sql
@@ -0,0 +1,14 @@
+-- creating the table to start
+
+CREATE TABLE jeopardy (
+	qid INTEGER,
+	topic TEXT,
+	level INTEGER,
+	prompt TEXT,
+	answer TEXT
+);
+
+
+-- import the data from a spread sheet instead of hard coding everything
+.mode csv
+.import jeopardy.csv jeopardy
